2d2e513be0de9a2f6f6deb3032afcd4a2e3495d5
Mohd Khizar Rasheed <161632919+mohdkhizarrasheed-7@users.noreply.github.com>
2026-09-08 12:31:48 +0530

    Add Spring Security authentication and session handling

--- src/main/java/com/learning/Security/controller/HomeController.java
+++ src/main/java/com/learning/Security/controller/HomeController.java
@@ -3,12 +3,14 @@ package com.learning.Security.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class HomeController
 {
 	@GetMapping("/")
-	public String greet()
+	public String greet(HttpServletRequest request)
 	{
-		return "Welcome to spring Security";
+		return "Welcome to spring Security" + " " + request.getSession().getId();
 	}
 }
